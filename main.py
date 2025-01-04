a98b3b3bed65b89ccfc8eed6ca33ea2972ca880d
synthmind027 <synthmind027@gmail.com>
2025-01-04 09:38:37 +0900

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((640,480))
 pygame.mouse.set_visible(False)
 
 FPS = 300
+C = (0,255,255)
 G = (0,255,0)
 ts = time.time()
 mouse = (0,0)
@@ -14,18 +15,54 @@ cursor_trail = []
 
 
 while True:
-	e = pygame.event.wait(30) # 30 ms
+
+
+	e = pygame.event.wait(30) # timeout 30 ms
+	
 	if e.type == pygame.QUIT:
 		break
+	
 	elif e.type == pygame.MOUSEMOTION:
 		mouse = e.pos
 		cursor_trail.append((mouse,time.time()))
 	
+	elif e.type == pygame.MOUSEBUTTONDOWN:
+		print('dn')
+		e.pos
 
+	elif e.type == pygame.MOUSEBUTTONUP:
+		print('up')
+		e.pos
+
+	elif e.type == pygame.KEYDOWN:
+		if e.unicode != '':
+			if e.scancode == 1:
+				print('ESC')
+			if e.scancode == 40:
+				print('enter')
+			elif e.scancode == 42:
+				print('backspace')
+			# 43 as tab
+			# 44 as space
+			print(e.unicode, ord(e.unicode), e.scancode)
+
+
+
+
+
+
+	# render
 	if ts + 1/FPS > time.time():
 		continue
 
+	# Draw background
 	screen.fill((0,0,0))
+
+
+
+
+
+	# Draw cursor trail
 
 	trail_thre = 10
 	trail_spacing = 10
@@ -54,12 +91,20 @@ while True:
 			q = [0, 0]
 			q[0] = p[0] - trail_size * norm[0] + trail_size * perp[0]
 			q[1] = p[1] - trail_size * norm[1] + trail_size * perp[1]
-			pygame.draw.aaline(screen, G, p, q)
+			pygame.draw.aaline(screen, C, p, q)
 			q = [0, 0]
 			q[0] = p[0] - trail_size * norm[0] - trail_size * perp[0]
 			q[1] = p[1] - trail_size * norm[1] - trail_size * perp[1]
-			pygame.draw.aaline(screen, G, p, q)
+			pygame.draw.aaline(screen, C, p, q)
 
-	pygame.draw.rect(screen, G, (mouse[0],mouse[1],3,3))
+
+
+
+
+	# Draw cursor
+
+	pygame.draw.rect(screen, C, (mouse[0],mouse[1],3,3))
 	
+
+
 	pygame.display.flip()
